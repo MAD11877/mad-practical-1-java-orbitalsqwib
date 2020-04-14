929e6891cc23d152316954e8be17dcb262ec7c57
OrbitalSqwib <21305518+orbitalsqwib@users.noreply.github.com>
2020-04-14 14:10:29 +0800

    Answer Question 5

--- Question5.java
+++ Question5.java
@@ -27,6 +27,41 @@ public class Question5
      */
      
     Scanner in = new Scanner(System.in);
+
+    System.out.print("Please enter an integer representing the number of integers to be entered: ");
+    int n = in.nextInt();
+
+    List<Integer> intList = new ArrayList<>();
+    List<Integer> counter = new ArrayList<>();
+
+    for(int i=0;i<n;i++) {
+      System.out.print(String.format("Enter integer %d: ", i+1));
+      int input = in.nextInt();
+      
+      if (!intList.contains(input)) {
+        //add new numbers to distinc intList and init counter at the same time
+        intList.add(input);
+        counter.add(1);
+      } else {
+        // if already existing, increment counter for corresponding int
+        int counterIndex = intList.indexOf(input);
+        counter.set(counterIndex, counter.get(counterIndex)+1);
+      }
+    }
+
+    // from here on, just get highest counter and print associated distinct int from intList
+    
+    int max = counter.get(0);
+    int index = 0;
+
+    for(int i=1;i<counter.size();i++) {
+      if (counter.get(i) > max) {
+        max = counter.get(i);
+        index = i;
+      }
+    }
+
+    System.out.println(String.format("The mode of the sequence is %d", intList.get(index)));
     
   }
 }
