505eea76145e5f1a66fefde62238fe2c05313a2e
OrbitalSqwib <21305518+orbitalsqwib@users.noreply.github.com>
2020-04-14 13:17:59 +0800

    Test Answer

--- Question1.java
+++ Question1.java
@@ -2,10 +2,6 @@ public class Question1
 {
   public static void main(String[] args)
   {
-    /**
-     * Print "Hello World!"
-     * 
-     * Hint: System.out.println(...)
-     */
+    System.out.println("Hello World!")
   }
 }
